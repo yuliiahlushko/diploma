167cfaadfa92915f1c8761186c42b1b28ea328bf
yuliiahlushko <glushkoyulia9992@gmail.com>
2024-04-10 16:18:19 +0300

    add display the picture when loading

--- src/main/java/com/example/excursionPlanning/controllers/web/UserController.java
+++ src/main/java/com/example/excursionPlanning/controllers/web/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
 import java.util.Base64;
